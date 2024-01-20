688d9511fad5858c1fd6bad14da8753a50de5089
Samuel Wemimo <wemsamuel@gmail.com>
2024-01-20 13:26:04 -0700

    wip: changing server connection to graphQL apollo

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,26 @@
-import express, { Application, Request, Response } from 'express';
+import { ApolloServer, gql } from 'apollo-server-express';
+import express, { Application } from 'express';
+
+const typeDefs = gql`
+  type Query {
+    url: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    url: () => 'http/www.google.com',
+  },
+};
+
+// as any is a work around because applyiddleware doesnt take type  Application for some reason
+const server = new ApolloServer({ typeDefs, resolvers }) as any;
 
 const app: Application = express();
-const port: number = 3000;
+const PORT: number | string = process.env.PORT || 3000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Load Server');
-});
+server.applyMiddleware({ app });
 
-app.listen(port, () => {
-  console.log(`App is Listening on port ${port}`);
+app.listen({ PORT }, () => {
+  console.log(`App is Listening on port ${PORT}`);
 });
